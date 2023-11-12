67e8e2f77733ce48c3a4f80181ab76d963a52223
rimcode6 <114460369+rimcode6@users.noreply.github.com>
2023-11-12 01:18:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
 import json
+from datetime import datetime
+import pytz  # To handle time zones
+import googlemaps  # You will need to install this package
+import os  # For reading environment variables
 
 app = Flask(__name__)
+
+# Initialize Google Maps client with API key from environment variable
+gmaps = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY', 'AIzaSyBEALSB8ogqVg-lr36gYfR-cFMRxZd3btc'))
 
 @app.route('/')
 def index():
@@ -11,77 +18,47 @@ def index():
 def get_stations():
     user_location = request.form['location']  # Assuming the user inputs their location
 
-    # Read data from JSON file instead of making an API call
     try:
         with open('stations_data.json', 'r') as file:
             stations_data = json.load(file)
-        
-        # Process stations_data to filter based on user_location and other criteria
-        # This logic depends on how you want to process the data
 
-        return render_template('stations.html', stations=stations_data)  # Create a 'stations.html' template
+        if not isinstance(stations_data, list) or not all(isinstance(station, dict) for station in stations_data):
+            return "Invalid data format in JSON file"
+
+        # Filter for stations in San Francisco
+        sf_stations = [station for station in stations_data if station.get('city') == 'San Francisco']
+
+        # Add wait time and Google Maps link
+        for station in sf_stations:
+            station['wait_time'] = calculate_wait_time(station.get('update_time'))
+            station['google_maps_link'] = get_directions_link(user_location, station.get('address'))
+
+        return render_template('stations.html', stations=sf_stations)
     except FileNotFoundError:
         return "JSON file not found"
     except json.JSONDecodeError:
         return "Error in reading the JSON file"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def calculate_wait_time(update_time_str):
+    if not update_time_str:
+        return None
+    try:
+        update_time = datetime.fromisoformat(update_time_str)
+        current_time = datetime.now(pytz.timezone('America/Los_Angeles'))  # San Francisco timezone
+        wait_time = current_time - update_time
+        return wait_time.seconds // 60  # Convert to minutes
+    except ValueError:
+        return None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''from flask import Flask, render_template, request
-import requests
-
-app = Flask(__name__)
-
-# Replace 'your_api_key' with your actual Intrix API key
-API_KEY = 'your_api_key'
-
-@app.route('/')
-def index():
-    return render_template('index.html')  # You'll need to create an 'index.html' template
-
-@app.route('/get_stations', methods=['POST'])
-def get_stations():
-    user_location = request.form['location']  # Assuming the user inputs their location
-
-    # API endpoint to get fuel stations data. Replace with the actual Intrix API endpoint
-    url = f"https://api.intrix.com/getStations?location={user_location}&radius=5&apikey={API_KEY}"
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        stations_data = response.json()
-        # Parse stations_data to extract required information
-        # e.g., wait time, brand, price, CO2 emission factors
-        # This depends on the structure of the response from the Intrix API
-
-        return render_template('stations.html', stations=stations_data)  # Create a 'stations.html' template
-    else:
-        return "Error fetching data from the API"
+def get_directions_link(user_location, station_address):
+    if not user_location or not station_address:
+        return None
+    try:
+        directions_result = gmaps.directions(user_location, station_address)
+        return directions_result[0]['overview_polyline']['points']  # Simplified representation of the route
+    except Exception as e:
+        print(f"Error getting directions: {e}")
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
-    '''
